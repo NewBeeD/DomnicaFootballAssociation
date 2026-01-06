efef8ae6c7ba0670e7d4f45464847d4ed395d7a2
Danphil Daniel <danieldanphil@gmail.com>
2026-01-06 12:32:39 -0400

    Update: This is the new Main

--- src/GamePrediction/pages/MyPredictionsPage.jsx
+++ src/GamePrediction/pages/MyPredictionsPage.jsx
@@ -14,6 +14,7 @@ import {
   Tabs,
   Tab,
 } from '@mui/material';
+
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../../config/firebaseConfig';
 import { usePredictions } from '../hooks/usePrediction';
